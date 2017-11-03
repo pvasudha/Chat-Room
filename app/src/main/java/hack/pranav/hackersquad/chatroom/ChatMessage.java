b82df147d5259b6426e374f5ed550b369fca8d58
InfernalVortex <pranz10@gmail.com>
2017-11-02 21:17:35 -0400

    commit chat service

---
+++ app/src/main/java/hack/pranav/hackersquad/chatroom/ChatMessage.java
@@ -0,0 +1,43 @@
+package hack.pranav.hackersquad.chatroom;
+
+import java.util.Date;
+
+public class ChatMessage {
+    private String messageText;
+    private String messageUser;
+    private long messageTime;
+
+    public ChatMessage() {
+        messageTime = new Date().getTime();
+    }
+
+    public ChatMessage(String messageText, String messageUser) {
+        this.messageText = messageText;
+        this.messageUser = messageUser;
+        messageTime = new Date().getTime();
+    }
+
+    public String getMessageText() {
+        return messageText;
+    }
+
+    public void setMessageText(String messageText) {
+        this.messageText = messageText;
+    }
+
+    public String getMessageUser() {
+        return messageUser;
+    }
+
+    public void setMessageUser(String messageUser) {
+        this.messageUser = messageUser;
+    }
+
+    public long getMessageTime() {
+        return messageTime;
+    }
+
+    public void setMessageTime(long messageTime) {
+        this.messageTime = messageTime;
+    }
+}
